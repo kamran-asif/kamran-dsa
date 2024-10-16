db2408f91fe23a1921c17ed1bc121f58d10734d4
kamran-asif <mdkamranasif789@gmail.com>
2024-10-16 18:26:17 +0530

    Time: 20 ms (71.89%), Space: 26.9 MB (58.86%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,22 @@
+
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, -1); 
+        stack<int> stk;
+        
+        for (int i = 2 * n - 1; i >= 0; --i) {
+              while (!stk.empty() && nums[stk.top()] <= nums[i % n]) {
+                stk.pop();
+            }
+            if (!stk.empty()) {
+                res[i % n] = nums[stk.top()];
+            }
+            stk.push(i % n);
+        }
+        
+        return res;
+    }
+};
