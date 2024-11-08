66e3b80a60b24e16f4b34ca1ab41d2ac74712d20
kamran-asif <mdkamranasif789@gmail.com>
2024-11-09 03:19:29 +0530

    Time: 0 ms (100%), Space: 7.7 MB (35.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,14 +4,14 @@ public:
   int climbStairs(int n) {
         if(n==1||n==2)
             return n;
-        vector<int>t(n+1);
+        vector<int>dp(n+1,-1);
        //t[i] = total ways to climb i stairs
-        t[0]=0;
-        t[1]=1;
-        t[2]=2;
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
         for(int i=3;i<=n;i++) {
-            t[i]=t[i-1]+t[i-2];
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return t[n];
+        return dp[n];
     }
 };
