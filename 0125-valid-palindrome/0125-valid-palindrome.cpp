b3c9cc25d04ee1277f5e79ccb890a12418f7bb02
kamran-asif <mdkamranasif789@gmail.com>
2024-11-16 19:58:24 +0530

    Time: 6 ms (8.42%), Space: 9.7 MB (10.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,14 +4,14 @@ public:
 
         string orig = "";
         string revstr = "";
-        // remove unnecessary
+        
         for (auto it : s) {
             
             if ((it >= 'a' && it <= 'z') || (it >= 'A' && it <=                 'Z') || (it >= '0' && it <= '9')) {
                 orig += it;
             }
         }
-        //make small
+    
         for (int itr = 0; itr < orig.size(); itr++) {
 
             char curr = orig[itr];
@@ -20,11 +20,10 @@ public:
             }
         } 
 
-        // reverse
         for (int i = orig.size() - 1; i >= 0; i--) {
             revstr += orig[i];
         }
-        // compare
+     
         if (revstr != orig)
             return false;
         return true;
