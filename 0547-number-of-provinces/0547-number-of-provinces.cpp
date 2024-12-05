2c7f4bd0d3de3967019a5e45cd0bff14ffc4fc74
kamran-asif <mdkamranasif789@gmail.com>
2024-12-05 19:30:57 +0530

    Time: 0 ms (100%), Space: 20.9 MB (13.03%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    void dfs(int node, vector<int> adjLs[], vector<int>& vis) {
+    void dfs(int node, vector<int> adjLs[],vector<int>&vis) {
         vis[node] = 1;
         for (auto it : adjLs[node]) {
             if (!vis[it]) {
