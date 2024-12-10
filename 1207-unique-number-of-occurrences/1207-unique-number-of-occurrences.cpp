3f46a29458622c6de5350c24bccb2131d690519e
kamran-asif <mdkamranasif789@gmail.com>
2024-12-10 19:24:26 +0530

    Time: 0 ms (100%), Space: 12.1 MB (8.49%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -3,15 +3,19 @@ public:// 1st approach map m dal denge occurences ko or cnt krlenge ;key m arr k
 //uske baad set m daldenge occrnse ko agr unique hua to true ni to false 
     bool uniqueOccurrences(vector<int>& arr) {
         unordered_map<int,int>mp;
+        //map m value daal rhe hai array s 
         for(int &x: arr){
             mp[x]++;
         }
         unordered_set<int>st;
+        //map s set m daal rhe hai 
         for(auto &it: mp){
+
             int freq=it.second;//it.first me number hoga
+            // check kr rhe hai ki freq agr same nhi hai to
             if(st.find(freq)!=st.end())
                 return false;
-                st.insert(freq);
+                st.insert(freq);//set m freq daldo
             
           
         }
