432c6bd90cd5c305449d2187b8b5538566deb4cc
kamran-asif <mdkamranasif789@gmail.com>
2024-12-31 12:43:49 +0530

    Time: 3 ms (75.82%), Space: 18.3 MB (49.61%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,45 +1,41 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        // Adjacency list representation
-        vector<int> adj[numCourses];
-        for (auto it : prerequisites) {
+    vector<int> findOrder(int numCourses,vector<vector<int>>& prerequisites) {
+        //list ko represnt krte hai aise
+        vector<int>adj[numCourses];
+        for (auto it:prerequisites) {
             adj[it[1]].push_back(it[0]);
         }
-
-        // Initialize indegree array
-        vector<int> indegree(numCourses, 0);
-        for (int i = 0; i < numCourses; i++) {
-            for (auto it : adj[i]) {
+//in degree array
+        vector<int> indegree(numCourses,0);
+        for (int i=0;i<numCourses;i++) {
+            for (auto it:adj[i]) {
                 indegree[it]++;
             }
         }
-
-        // Queue to store nodes with indegree 0
+// queue use kr rhe hai indegree store krne k liy 
         queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
+        for (int i=0;i<numCourses;i++) {
+            if (indegree[i]==0) {
                 q.push(i);
             }
         }
-
-        // Perform Kahn's Algorithm for Topological Sort
+//khan's algo 
         vector<int> topo;
         while (!q.empty()) {
-            int node = q.front();
+            int node=q.front();
             q.pop();
             topo.push_back(node);
 
-            for (auto it : adj[node]) {
+            for (auto it:adj[node]) {
                 indegree[it]--;
-                if (indegree[it] == 0) {
+                if (indegree[it]==0) {
                     q.push(it);
                 }
             }
         }
-
-        // If the topo sort includes all courses, return it; otherwise, return an empty array
-        if (topo.size() == numCourses) return topo;
+// agr topo sort me sare courses hai to return krdo wrna empty retrn kro
+        if (topo.size()==numCourses)return topo;
         return {};
     }
 };
