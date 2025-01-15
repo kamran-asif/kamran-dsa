9b6838da3aec3385a8ad79a68350628df2c4988e
kamran-asif <mdkamranasif789@gmail.com>
2025-01-15 21:43:49 +0530

    Time: 50 ms (54.47%), Space: 94.1 MB (39.55%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-static bool comparator(vector<int>val1,vector<int>val2){
-    return val1[1]<val2[1];
-}
+    static bool comparator(vector<int>& val1, vector<int>& val2) {
+        return val1[1] < val2[1]; // Sort intervals by their end times
+    }
+
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-     sort(intervals.begin(), intervals.end(), comparator);
-     int cnt=1;
-     int lastendtime=intervals[0][1];
-     int n=intervals.size();
-     for(int i=1;i<n;i++){
-          if(intervals[i][0]>=lastendtime)
-          cnt=cnt+1;
-          lastendtime=intervals[i][1];
-     }  
-     return n-cnt;
+        if (intervals.empty()) return 0; // edge case no intervals
+        
+        // sort intervals by their end times
+        sort(intervals.begin(), intervals.end(), comparator);
+
+        int cnt = 1; //cnt of non-overlapping intervals
+        int lastEndTime = intervals[0][1]; // initialize with the end time of the first interval
+
+        // traverse the sorted intervals
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] >= lastEndTime) { 
+                // if the current interval does not overlap
+                cnt++;
+                lastEndTime = intervals[i][1]; // update the last end time
+            }
+        }
+
+        return intervals.size() - cnt; // overlapping intervals = total - non-overlapping
     }
 };
