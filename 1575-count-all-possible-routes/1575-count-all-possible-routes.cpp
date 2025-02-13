a4f5e4b4f7eae734ca6afc1ca1887118aafa1c46
kamran-asif <mdkamranasif789@gmail.com>
2025-02-13 21:40:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+int n;
+const int MOD=1e9+7;
+int t[101][201];
+int solve(vector<int>&locations,int i,int finish,int fuel){
+    if(fuel<0)
+    return 0;
+    if(t[i][fuel]!=-1)
+    return t[i][fuel];
+    int ans=0;
+    if(i==finish){
+        ans+=1;
+
+    }
+    for(int j=0;j<n;j++){
+        if(i!=j){
+            int remaining_fuel=fuel-abs(locations[i]-locations[j]);
+            ans=ans+solve(locations,j,finish,remaining_fuel);
+        }
+      
+    }
+     return t[i][fuel]= ans;
+}
+    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+     n=locations.size();
+     memset(t,-1,sizeof(t));
+     return solve(locations,start,finish,fuel);   
+    }
+};
