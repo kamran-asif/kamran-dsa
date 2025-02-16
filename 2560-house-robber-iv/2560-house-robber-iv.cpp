0302cc2d8ae9c3bf04c7108721b7397f6d435c36
kamran-asif <mdkamranasif789@gmail.com>
2025-02-16 14:36:06 +0530

    Time: 23 ms (69.71%), Space: 60.9 MB (67.03%) - LeetHub

--- 2560-house-robber-iv/2560-house-robber-iv.cpp
+++ 2560-house-robber-iv/2560-house-robber-iv.cpp
@@ -5,31 +5,31 @@ class Solution {
 public:
   
     int check(int mid, vector<int>& nums) {  
-        int take = 0;  // Kitne houses rob ho sakte hain, iska count rakhne ke liye
+        int take = 0;      // kitne houses rob ho sakte hain, iska count rakhne ke liye
         int n = nums.size();  
 
-        for (int i = 0; i < n; i++) {  // Sare houses check karenge
-            if (nums[i] <= mid) {  // Agar house ki value mid se chhoti ya barabar hai
-                i++;  // Ek house skip karna hai, kyunki adjacent houses nahi le sakte
-                take++;  // Ek house rob kar liya
+        for (int i = 0; i < n; i++) {   // sare houses check karenge
+            if (nums[i] <= mid) {   // agar house ki value mid se chhoti ya barabar hai
+                i++;     // ek house skip karna hai, kyunki adjacent houses nahi le sakte
+                take++;    // ek house rob kar liya
             }
         }
-        return take;  // Total kitne houses rob kiye, ye return karenge
+        return take;     // total kitne houses rob kiye, ye return karenge
     }
 
     int minCapability(vector<int>& nums, int k) {
         int l = 1;  
         int r = pow(10, 9);  
-        int ans = -1;  // answer ko initially -1 mark krenge
+        int ans = -1;        // answer ko initially -1 mark krenge
 
         while (l <= r) {  
             int mid = l + (r - l) / 2;
 
-            if (check(mid, nums) >= k) {  // agar mid value se k ya zyada houses rob ho skte hai
-                ans = mid;  // possible answr store krenge
-                r = mid - 1;  // chhoti value dhondhenge to kam capability me kam ho ske
+            if (check(mid, nums) >= k) {     // agar mid value se k ya zyada houses rob ho skte hai
+                ans = mid;          // possible answr store krenge
+                r = mid - 1;        // chhoti value dhondhenge to kam capability me kam ho ske
             } else {  
-                l = mid + 1;  // nhi ho ska, to bdi value try karenge
+                l = mid + 1;         // nhi ho ska, to bdi value try karenge
             }
         }
         return ans;  
