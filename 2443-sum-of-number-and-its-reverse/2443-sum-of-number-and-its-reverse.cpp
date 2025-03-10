3f50284dc00d0e218b07e53e56029dd7c9c30ec8
kamran-asif <mdkamranasif789@gmail.com>
2025-03-11 00:36:06 +0530

    Time: 884 ms (22.54%), Space: 10.1 MB (30.06%) - LeetHub

---
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool sumOfNumberAndReverse(int num) {
+         for (int i = 0; i <= num; i++) {
+
+            string s = to_string(i);
+        
+            reverse(s.begin(), s.end());
+           
+            int rev = stoi(s);
+            
+            if (i + rev == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
