1762fe9e3d7222510054a1213f9244a6a9e94d9d
kamran-asif <mdkamranasif789@gmail.com>
2025-03-11 00:24:05 +0530

    Time: 77 ms (78.73%), Space: 13 MB (68.7%) - LeetHub

---
+++ 0313-super-ugly-number/0313-super-ugly-number.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        vector<int> dp(n);
+        dp[0] = 1;
+        int m = primes.size();
+        
+        // min-Heap to get the next minimum ugly number efficiently
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        
+        // store (value, prime_index)
+        for(int i = 0; i < m; i++) {
+            pq.push({primes[i], i});
+        }
+        
+        vector<int> pointers(m, 0);
+
+        for(int i = 1; i < n; i++) {
+            // get the next smallest ugly number from the heap
+            dp[i] = pq.top().first;
+            
+            // update all pointers whose value matches the top of heap
+            while(!pq.empty() && pq.top().first == dp[i]) {
+                int prime_index = pq.top().second;
+                pq.pop();
+                
+                // move the pointer forward and push the new ugly number
+                pointers[prime_index]++;
+                pq.push({(long long)dp[pointers[prime_index]] * primes[prime_index], prime_index});
+            }
+        }
+
+        return dp[n-1];
+    }
+};
