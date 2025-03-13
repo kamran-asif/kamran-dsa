c77be8334cddbfba51923f79f06dac7b068a5ce8
kamran-asif <mdkamranasif789@gmail.com>
2025-03-13 08:14:01 +0530

    Time: 92 ms (53.14%), Space: 94.8 MB (54.14%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -4,23 +4,23 @@ public:
         int n = prices.size();
         if(n == 0) return 0;
         
-        // dp[i][0] -> Maximum profit if no stock is held on day i
-        // dp[i][1] -> Maximum profit if a stock is held on day i
+        // dp[i][0] -> maximum profit if no stock is held on day i
+        // dp[i][1] -> maximum profit if a stock is held on day i
         vector<vector<int>> dp(n, vector<int>(2, 0));
         
-        // Base case
-        dp[0][0] = 0;              // No stock on day 0
-        dp[0][1] = -prices[0];     // Bought stock on day 0
+        // base case
+        dp[0][0] = 0;              // no stock on day 0
+        dp[0][1] = -prices[0];     // bought stock on day 0
         
         for(int i = 1; i < n; i++) {
-            // Maximum profit without any stock today
+            // maximum profit without any stock today
             dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i] - fee);
             
-            // Maximum profit if you are holding a stock today
+            // maximum profit if you are holding a stock today
             dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i]);
         }
         
-        // The maximum profit will be when we do not have any stock on the last day
+        // the maximum profit will be when we do not have any stock on the last day
         return dp[n-1][0];
     }
 };
