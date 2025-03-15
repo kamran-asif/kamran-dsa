2294f9be536c578cbd18bfe4c960adde06ca9164
kamran-asif <mdkamranasif789@gmail.com>
2025-03-15 12:49:31 +0530

    Time: 0 ms (100%), Space: 7.8 MB (69.06%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     int countNumbersWithUniqueDigits(int n){
-    if (n == 0) return 1;
-      int total=10;//f(1)=10(digt 0->9)
-        int product=9,availableDigits=9;
-        for (int i=2;i<=n;i++){
-            product*=availableDigits;
-            availableDigits--;// rdce avilble chics
-            total+=product;
+ 
+   
+        if (n == 0) return 1;  
+        vector<int> dp(n + 1, 0);
+        dp[1] = 10;  
+        
+        int total = dp[1];
+        int product = 9, availableDigits = 9;
+        
+        for (int i = 2; i <= n; i++) {
+            product *= availableDigits;  // dp[i] = dp[i-1] * avilble chioices
+            availableDigits--;  
+            dp[i] = product;
+            total += dp[i];  // sum all dp[i] valus
         }
+        
         return total;
     }
 };
+
