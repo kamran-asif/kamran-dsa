22c1a72622099003fef9597008d3fa386cd95745
kamran-asif <mdkamranasif789@gmail.com>
2025-04-07 17:30:49 +0530

    Time: 1654 ms (5%), Space: 203.4 MB (7.36%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,26 +1,23 @@
 class Solution {
-public:// revisit it
-      /*        array
-       //        /     \         
-    // include the element       exclude the element*/
-  bool recurrsion(vector<int>&nums,int i,int sum,unordered_map<string,bool>&map){
-    string s=to_string(i)+to_string(sum);
-    if(sum<0) return false;
-         if(sum==0)return true;
-         if(i==nums.size())return false;
-         map[s]=recurrsion(nums,i+1,sum-nums[i],map)||recurrsion(nums,i+1,sum,map);
-         return map[s];
-}
-
+public:// revisit
+    bool recursion(vector<int> &nums,int i,int sum,unordered_map<string,bool> &map)
+    {   string s = to_string(i)+to_string(sum);
+        if(map.find(s)!=map.end()) return map[s];
+        if(sum<0) return false;
+        if(sum==0) return true;
+        if(i==nums.size()) return false;
+        map[s] = recursion(nums,i+1,sum-nums[i],map) || recursion(nums,i+1,sum,map);
+        return map[s];
+    }
     bool canPartition(vector<int>& nums) {
-         int n=nums.size();
-         int sum=0;
-         for(int i=0;i<n;i++){
-             sum+=nums[i];
-         }
-         if(sum%2!=0)return false;
-       
-        unordered_map<string, bool> map;     
-        return recurrsion(nums,0,sum/2,map);//starting index is 0
+        int n = nums.size();
+        int sum = 0;
+	    for(int i=0;i<n;i++)
+        {
+	        sum+=nums[i];
+	    }
+        if(sum%2!=0) return false;
+        unordered_map<string,bool> map;
+        return recursion(nums,0,sum/2,map);
     }
 };
