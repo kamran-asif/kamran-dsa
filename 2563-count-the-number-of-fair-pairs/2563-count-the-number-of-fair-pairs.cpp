54db0b190388dd540efd6349826eebe29356beef
kamran-asif <mdkamranasif789@gmail.com>
2025-04-19 16:45:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+     unordered_map<int>mp;
+     for(int i=0;i<nums.size();i++) {
+        if(nums[i]+nums[j]<=upper){
+            
+        }
+     }  
+    }
+};
