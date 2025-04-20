9b46cf3e134111c15cfffff38a67db24d54a7f8f
kamran-asif <mdkamranasif789@gmail.com>
2025-04-20 11:14:57 +0530

    Time: 60 ms (39.28%), Space: 60.4 MB (38.5%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -1,11 +1,29 @@
 class Solution {
 public:
-    long long countFairPairs(vector<int>& nums, int lower, int upper) {
-     unordered_map<int>mp;
-     for(int i=0;i<nums.size();i++) {
-        if(nums[i]+nums[j]<=upper){
-            
+    long long lower_bound(vector<int>& nums, int low, int high, int element) {
+        while (low <= high) {
+            int mid = low + ((high - low) / 2);
+            if (nums[mid] >= element) {
+                high = mid - 1;
+            } else
+                low = mid + 1;
         }
-     }  
+        return low;
+    }
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        sort(nums.begin(), nums.end());
+        long long ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int low =
+                lower_bound(nums, i + 1, nums.size() - 1, lower - nums[i]);
+
+            int high =
+                lower_bound(nums, i + 1, nums.size() - 1, upper - nums[i] + 1);
+
+            // their diffrnce givs the nmber of elments with sum in the
+            // given rnge.
+            ans += 1LL * (high - low);
+        }
+        return ans;
     }
 };
