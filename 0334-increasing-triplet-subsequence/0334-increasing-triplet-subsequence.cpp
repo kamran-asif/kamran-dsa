c50b77c3480bb0765b7e403c9f5d314315f40121
kamran-asif <mdkamranasif789@gmail.com>
2025-05-24 10:37:26 +0530

    Time: 11 ms (5.5%), Space: 115.7 MB (24.1%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -2,16 +2,22 @@ class Solution {
 public://revisit
     bool increasingTriplet(vector<int>& nums) {
         int n=nums.size();
-        vector<int>lis;
+        vector<int>lis;//simulate the longest increasing subsequence
         for(int i=0;i<n;i++){
+            //find the 1st index of 1st element in lis which is not less than nums[i]
+            //this is the posn where nums[i] can be placed to maintain sorted order
             int lb=lower_bound(lis.begin(),lis.end(),nums[i])-lis.begin();
             if(lb==lis.size()){
+                //if nums[i] is greater than all elements in'lis' which is not less than nums[i]
+                //this is the posn where nums[i] can be placed to maintain sorted order
                 lis.push_back(nums[i]);
                 }else{
                 lis[lb]=nums[i];
             }
             
         }
-        return lis.size()>=3;
+        //if we managed to build subsequence of length 3 return true
+        // exist a subsequence of triplet subsequence
+        return lis.size()>=3; 
     }
 };
